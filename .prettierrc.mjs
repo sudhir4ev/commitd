bde4557d0cecb0e15914abc430eeead976648413
Sudhir <rihdus.io+dev@gmail.com>
2026-02-21 12:36:13 +0800

    Add Prettier configuration and ignore files, enhance project structure
    - Introduced .prettierignore to exclude build and dependency directories.
    - Added .prettierrc.mjs for Prettier configuration with Astro plugin support.
    - Updated package.json to include Prettier and Husky for code formatting and pre-commit hooks.
    - Refactored various components and pages to ensure consistent formatting.
    - Enhanced project structure with VSCode tasks for easier development.
    - Minor adjustments to existing components for improved readability and maintainability.
    
    This commit improves code quality and project setup for better development experience.

---
+++ .prettierrc.mjs
@@ -0,0 +1,23 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+}
+
+export default {
+  ...config,
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+}
