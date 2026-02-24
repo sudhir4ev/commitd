94a9e90f78aeb4b5465be1580f8715bf29cc8cf9
Sudhir <rihdus.io+dev@gmail.com>
2026-02-24 11:46:12 +0800

    mermaid render

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import sitemap from '@astrojs/sitemap'
 import mdx from '@astrojs/mdx'
 import pagefind from 'astro-pagefind'
 import tailwindcss from '@tailwindcss/vite'
+import { rehypeMermaidCLI } from 'rehype-mermaid-cli'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro.app',
-  integrations: [sitemap(), mdx(), pagefind()],
+  integrations: [
+    sitemap(),
+    mdx({ rehypePlugins: [rehypeMermaidCLI] }),
+    pagefind(),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
@@ -15,5 +20,10 @@ export default defineConfig({
     shikiConfig: {
       theme: 'css-variables',
     },
+    syntaxHighlight: {
+      type: 'shiki',
+      excludeLangs: ['mermaid'],
+    },
+    rehypePlugins: [rehypeMermaidCLI],
   },
 })
